7b5546a0f97f176577c110c132e9e3a2aec9ffc0
Mato Ilic <info@matoilic.ch>
2015-07-11 00:40:52 +0200

    use shorthand $ for element selection

--- test/alert-dialog-test.js
+++ test/alert-dialog-test.js
@@ -14,18 +14,18 @@ describe('alter dialog', function() {
     it('should show an alert', function() {
         browser.executeScript(openAlert);
 
-        expect(element(by.css('#testMessage')).isPresent()).toBeTruthy();
-        expect(element.all(by.css('.dijalog-button')).count()).toBe(1);
+        expect($('#testMessage').isPresent()).toBeTruthy();
+        expect($$('.dijalog-button').count()).toBe(1);
     });
 
     it('should hide the alert when clicking OK', function() {
         browser.executeScript(openAlert);
 
-        element(by.css('.dijalog-button-primary')).click();
+        $('.dijalog-button-primary').click();
 
         browser.wait(ec.not(ec.presenceOf($('#testMessage'))), config.defaultWaitTime);
 
-        expect(element(by.css('#testMessage')).isPresent()).toBeFalsy();
+        expect($('#testMessage').isPresent()).toBeFalsy();
     });
 
     it('should focus the OK button by default', function() {
@@ -34,7 +34,7 @@ describe('alter dialog', function() {
         browser.wait(ec.visibilityOf($('.dijalog-button-primary')), config.defaultWaitTime);
 
         expect(
-            element(by.css('.dijalog-button-primary')).getText()
+            $('.dijalog-button-primary').getText()
         )
         .toEqual(
             browser.driver.switchTo().activeElement().getText()
